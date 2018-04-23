21a97727ce3990620eefb3f52b891c5b33f17098
alexc <corotchi@gmail.com>
2018-04-22 21:53:53 -0400

    callback auth

--- src/auth/auth0-variables.js
+++ src/auth/auth0-variables.js
@@ -2,6 +2,6 @@ export const AUTH_CONFIG = {
     domain: 'bnchmark.auth0.com',
     clientId: 'BA4rafxlwdUFOpv2gdOfi7QK8z16QaVB',
     // callbackUrl: 'http://localhost:5000/callback'
-    callbackUrl: 'http://localhost:3000/callback'
-    // callbackUrl: 'https://test.bnchmark.com/callback'
+    // callbackUrl: 'http://localhost:3000/callback'
+    callbackUrl: 'https://test.bnchmark.com/callback'
 };
