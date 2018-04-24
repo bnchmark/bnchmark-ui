8538d7f7ff590f5d666bf8f78dbfb76d144bf099
alexc <corotchi@gmail.com>
2018-04-23 21:30:22 -0400

    fix login origin

--- src/auth/auth0-variables.js
+++ src/auth/auth0-variables.js
@@ -1,6 +1,7 @@
 export const AUTH_CONFIG = {
     domain: 'bnchmark.auth0.com',
     clientId: 'BA4rafxlwdUFOpv2gdOfi7QK8z16QaVB',
-    callbackUrl: 'http://localhost:3000/callback'
+    callbackUrl: `${window.location.origin}/callback`
+    // callbackUrl: 'http://localhost:3000/callback'
     // callbackUrl: 'https://test.bnchmark.com/callback'
 };
